9321359cb95c17c7a23717409386999b1fcaca43
ibra <ebrah210@gmail.com>
2020-12-31 20:24:37 -0600

    installed dependencies and started on keyboard component

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {Keyboard} from 'react-native-ui-lib';
+import Item from './keyboard/index';
+const KeyboardAccessoryView = Keyboard.KeyboardAccessoryView;
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Item/>
       <StatusBar style="auto" />
     </View>
   );
